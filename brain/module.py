4e592fd19733bde74e2b97045cdfd15520c2582a
Daniel Gruss <dsgruss@users.noreply.github.com>
2022-05-18 16:03:42 -0700

    Adds a very simple broadcast to determine the global state

--- brain/module.py
+++ brain/module.py
@@ -1,14 +1,12 @@
+import asyncio
 import netifaces
 import json
 import socket
-import threading
-import time
 import uuid
 
+from enum import Enum
 from itertools import chain
-from struct import unpack
 
-from brain import consensus
 
 class InputJack:
     def __init__(self, data_callback, params):
@@ -21,7 +19,6 @@ class InputJack:
         if self.state != state:
             self.state = state
             self._owning_module.update_patch()
-
 
     def is_patched(self) -> bool:
         # Returns True if another module is sending data to the jack
@@ -62,74 +59,26 @@ class Module:
     # Class to handle networking and discovery layers for each module
     inputs = []
     outputs = []
-
-    _network_interfaces = []
-    _sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-    directive_port = 12345
+    patch_port = 19874
 
     def __init__(self, name, patching_callback=None):
         # Initializes the module and allows for discovery by management requests
         self.name = name
         self.patching_callback = patching_callback
-        self._uuid = uuid.uuid4()
+        self.uuid = str(uuid.uuid4())
+        self.patch_state = PatchState.IDLE
 
-        print("Discovering network interfaces...")
+        self.sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 2)
+        self.sock.bind(("", self.patch_port))
 
-        for interface in netifaces.interfaces():
-            interfaces_details = netifaces.ifaddresses(interface)
-            if netifaces.AF_INET in interfaces_details:
-                self._network_interfaces.extend(interfaces_details[netifaces.AF_INET])
-
-        print(self._network_interfaces)
-        for interface in self._network_interfaces:
-            threading.Thread(
-                target=self._loop, args=(interface["addr"],), daemon=True
-            ).start()
-
-        self._consensus = consensus.Consensus(None, self._uuid)
-
-
-    def _loop(self, local_address):
-        # Thread that responds to identification and control commands
-
-        sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 2)
-
-        for _ in range(10):
-            try:
-                sock.bind((local_address, self.directive_port))
-                break
-            except OSError:
-                self.directive_port += 1
-                continue
-        else:
-            print("Unable to find open port.")
-            exit(-1)
-
-        env = {"name": self.name, "id": self._uuid, "inputs": [], "outputs": []}
-
-        print(f"Listening for directives on {local_address}:{self.directive_port}...")
-
-        while True:
-            msg, addr = sock.recvfrom(1500)
-            if msg.startswith(b"IDENTIFY"):
-                print("Identification command received.")
-                env.outputs = [o.params for o in self.outputs]
-                sock.sendto(bytes(json.dumps(env), "utf8"), addr)
-            elif msg.startswith(b"REQUEST"):
-                print("Patch mapping command received.")
-                directive, destination_address, destination_port, id = unpack(
-                    "!10s4shB", msg
-                )
-                address = (socket.inet_ntoa(destination_address), destination_port)
-                for o in self.outputs:
-                    if o.params.id == id:
-                        o.destinations.append(address)
-            elif msg.startswith(b"RESET"):
-                print("Reset command received.")
-                for o in self.outputs:
-                    o.destinations.clear()
+        self.protocol = PatchProtocol(
+            self.uuid, self.patch_port, self.update_patch_state
+        )
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(
+            loop.create_datagram_endpoint(lambda: self.protocol, sock=self.sock)
+        )
 
     def add_input(self, data_callback) -> InputJack:
         # Adds a new input to the module
@@ -147,4 +96,82 @@ class Module:
 
     def update_patch(self):
         # Trigger in update in the shared state
-        self._consensus.update([j.state for j in chain(self.inputs, self.outputs) if j.state])
+        self.protocol.update(
+            [j.state for j in chain(self.inputs, self.outputs) if j.state]
+        )
+
+    def update_patch_state(self, patch_state):
+        if self.patch_state != patch_state:
+            self.patch_state = patch_state
+            print(patch_state)
+
+
+class PatchState(Enum):
+    # Global state possibilities
+    IDLE = 0  # No buttons pushed across all modules
+    PATCH_ENABLED = 1  # One single button pushed
+    PATCH_TOGGLED = 2  # Two buttons pushed, consisting of an input and output
+    BLOCKED = 3  # Three or more buttons pushed or two of the same type
+
+
+class PatchProtocol(asyncio.DatagramProtocol):
+
+    states = {}
+    broadcast_addrs = []
+
+    def __init__(self, uuid, port, state_callback) -> None:
+        self.uuid = uuid
+        self.port = port
+        self.state_callback = state_callback
+
+        self.states[uuid] = []
+
+        for interface in netifaces.interfaces():
+            interfaces_details = netifaces.ifaddresses(interface)
+            if netifaces.AF_INET in interfaces_details:
+                for detail in interfaces_details[netifaces.AF_INET]:
+                    if detail["addr"] != "127.0.0.1":
+                        self.broadcast_addrs.append((detail["broadcast"], port))
+
+        super().__init__()
+
+    def connection_made(self, transport):
+        print("Connection made")
+        self.transport = transport
+
+    def datagram_send(self, json_msg):
+        print("=>")
+        print(json_msg)
+        payload = bytes(json.dumps(json_msg), "utf8")
+        for addr in self.broadcast_addrs:
+            self.transport.sendto(payload, addr)
+            print(addr)
+
+    def update(self, local_state):
+        # Updates the local state and triggers a global check-in
+        self.states[self.uuid] = local_state
+        self.datagram_send(
+            {"message": "UPDATE", "uuid": self.uuid, "state": local_state}
+        )
+        self.push_update()
+
+    def push_update(self):
+        self.state_callback(
+            PatchState(min(3, sum(len(v) for k, v in self.states.items())))
+        )
+
+    def datagram_received(self, data: bytes, addr) -> None:
+        try:
+            response = json.loads(data)
+        except json.JSONDecodeError:
+            return
+        if (
+            "message" not in response
+            or "uuid" not in response
+            or response["uuid"] == self.uuid
+        ):
+            return
+
+        if response["message"] == "UPDATE":
+            self.states[response["uuid"]] = response["state"]
+            self.push_update()
