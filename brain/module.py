0c1a562d0f487e75c208e879ffe9acf0cf0040a9
Daniel Gruss <dsgruss@users.noreply.github.com>
2022-05-19 14:18:37 -0700

    Adds initial rough pass on data transfer

--- brain/module.py
+++ brain/module.py
@@ -2,6 +2,7 @@ import asyncio
 import netifaces
 import json
 import logging
+import random
 import socket
 import uuid
 
@@ -32,19 +33,52 @@ class InputJack(Jack):
     def __init__(self, parent_module, data_callback, **kwargs):
         self.callback = data_callback
         self.params = kwargs
+
+        self.protocol = None
+
         super().__init__(parent_module)
+
+    def is_patched(self) -> bool:
+        return self.protocol is not None
+
+    def clear(self):
+        if self.is_patched():
+            self.endpoint.close()
+            self.sock.close()
+            self.protocol = None
+
+    def connect(self, address, port):
+        if self.is_patched():
+            self.clear()
+
+        self.sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 2)
+        self.sock.bind((address, port))
+
+        self.protocol = DataProtocol(self)
+        loop = asyncio.get_event_loop()
+        self.endpoint = loop.create_datagram_endpoint(
+            lambda: self.protocol, sock=self.sock
+        )
+        loop.create_task(self.endpoint)
+
+    def proto_callback(self, data):
+        self.callback(data)
 
 
 class OutputJack(Jack):
-    def __init__(self, parent_module, **kwargs):
+    def __init__(self, parent_module, address, **kwargs):
         self.params = kwargs
-        self.destinations = []
+
+        # For now we just pick a port, but this should be negotiated during device discovery
+        self.endpoint = (address, random.randrange(49152, 65535))
+        logging.info("Jack endpoint: " + str(self.endpoint))
+
         super().__init__(parent_module)
 
     def send(self, data: bytes):
-        rtp_header = bytes("############", "ASCII")
-        for loc in self.destinations:
-            self.parent_module._sock.sendto(rtp_header + data, loc)
+        # Currently sending all the data at all times
+        self.parent_module.sock.sendto(data, self.endpoint)
 
 
 class Module:
@@ -53,6 +87,7 @@ class Module:
     outputs = []
     patch_port = 19874
     preferred_broadcast = "10.255.255.255"
+    broadcast_addr = None
 
     def __init__(self, name, patching_callback=None):
         # Initializes the module and allows for discovery by management requests
@@ -74,26 +109,26 @@ class Module:
         if len(addresses) == 0:
             return
 
-        used_address = addresses[0]
+        self.broadcast_addr = addresses[0]
         for detail in addresses:
             if detail["broadcast"] == self.preferred_broadcast:
-                used_address = detail
+                self.broadcast_addr = detail
 
         # The socket is created manually here because the handler doesn't appear to appear to allow
         # an address reuse, even though we are using a broadcast
 
         self.sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 2)
-        self.sock.bind((used_address["addr"], self.patch_port))
+        self.sock.bind((self.broadcast_addr["addr"], self.patch_port))
 
         self.protocol = PatchProtocol(
             self.uuid,
-            used_address["broadcast"],
+            self.broadcast_addr["broadcast"],
             self.patch_port,
             self.update_patch_state,
         )
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(
+        loop.create_task(
             loop.create_datagram_endpoint(lambda: self.protocol, sock=self.sock)
         )
 
@@ -107,22 +142,50 @@ class Module:
 
     def add_output(self, **kwargs) -> OutputJack:
         # Adds a new output to the module
-        jack = OutputJack(self, id=len(self.outputs) + len(self.inputs), **kwargs)
+        jack = OutputJack(
+            self,
+            self.broadcast_addr["broadcast"],
+            id=len(self.outputs) + len(self.inputs),
+            **kwargs
+        )
         self.outputs.append(jack)
         return jack
 
     def update_patch(self):
         # Trigger in update in the shared state
         s = [{"id": j.params["id"], "type": "input"} for j in self.inputs if j.state]
-        s += [{"id": j.params["id"], "type": "output"} for j in self.outputs if j.state]
+        s += [
+            {
+                "id": j.params["id"],
+                "type": "output",
+                "address": j.endpoint[0],
+                "port": j.endpoint[1],
+            }
+            for j in self.outputs
+            if j.state
+        ]
         self.protocol.update(s)
 
-    def update_patch_state(self, patch_state):
+    def update_patch_state(self, patch_state, global_states):
         if self.patch_state != patch_state:
             self.patch_state = patch_state
             if self.patching_callback is not None:
                 self.patching_callback(patch_state)
             logging.info(patch_state)
+
+            if patch_state == PatchState.PATCH_TOGGLED:
+                if global_states[0]["type"] == "input":
+                    input = global_states[0]
+                    output = global_states[1]
+                else:
+                    input = global_states[1]
+                    output = global_states[0]
+                if input["uuid"] == self.uuid:
+                    self.make_connection(input, output)
+
+    def make_connection(self, input, output):
+        input_jack = [j for j in self.inputs if j.params["id"] == input["id"]][0]
+        input_jack.connect(self.broadcast_addr["addr"], output["port"])
 
 
 class PatchState(Enum):
@@ -131,6 +194,19 @@ class PatchState(Enum):
     PATCH_ENABLED = 1  # One single button pushed
     PATCH_TOGGLED = 2  # Two buttons pushed, consisting of an input and output
     BLOCKED = 3  # Three or more buttons pushed or two of the same type
+
+
+class DataProtocol(asyncio.DatagramProtocol):
+    def __init__(self, jack) -> None:
+        self.jack = jack
+
+        super().__init__()
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data: bytes, addr) -> None:
+        self.jack.proto_callback(data)
 
 
 class PatchProtocol(asyncio.DatagramProtocol):
@@ -148,7 +224,7 @@ class PatchProtocol(asyncio.DatagramProtocol):
         super().__init__()
 
     def connection_made(self, transport):
-        logging.info("Connection made")
+        logging.info("Patching broadcast connection made")
         self.transport = transport
 
     def datagram_send(self, json_msg):
@@ -175,16 +251,16 @@ class PatchProtocol(asyncio.DatagramProtocol):
                 all_states.append(a)
         logging.info(all_states)
         if len(all_states) >= 3:
-            self.state_callback(PatchState.BLOCKED)
+            self.state_callback(PatchState.BLOCKED, all_states)
         elif len(all_states) == 2:
             if all_states[0]["type"] == all_states[1]["type"]:
-                self.state_callback(PatchState.BLOCKED)
+                self.state_callback(PatchState.BLOCKED, all_states)
             else:
-                self.state_callback(PatchState.PATCH_TOGGLED)
+                self.state_callback(PatchState.PATCH_TOGGLED, all_states)
         elif len(all_states) == 1:
-            self.state_callback(PatchState.PATCH_ENABLED)
+            self.state_callback(PatchState.PATCH_ENABLED, all_states)
         else:
-            self.state_callback(PatchState.IDLE)
+            self.state_callback(PatchState.IDLE, all_states)
 
     def datagram_received(self, data: bytes, addr) -> None:
         try:
