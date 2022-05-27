b087da16c5f1e574b1d4d91ab2a805a1d5809afe
Daniel Gruss <dsgruss@users.noreply.github.com>
2022-05-26 20:09:03 -0700

    Fix missing return values

--- brain/parsers.py
+++ brain/parsers.py
@@ -23,10 +23,11 @@ class MessageParser:
     """Determines how the messages passed on the patching port get translated into raw bytes in the
     udp packets.
     """
+
     def parse_directive(self, data: bytes) -> Union[Message, None]:
         """Turns raw bytes into a ``Message``. Returns ``None`` if the message was unable to be
         parsed.
-        
+
         :param data: Raw data
 
         :return: The message object or ``None``
@@ -34,27 +35,28 @@ class MessageParser:
         try:
             response = json.loads(data)
         except json.JSONDecodeError:
-            return
+            return None
         if any(k not in response for k in ("message", "uuid")):
-            return
+            return None
 
         if response["message"] == "UPDATE":
             if "state" not in response:
-                return
+                return None
             state = LocalState(list(), list())
             for d in response["state"].get("inputs") or []:
                 if any(k not in d for k in ("id",)):
-                    return
+                    return None
                 state.held_inputs.append(HeldInputJack(response["uuid"], d["id"]))
             for d in response["state"].get("outputs") or []:
                 if any(k not in d for k in ("id", "color", "port")):
-                    return
+                    return None
                 state.held_outputs.append(
                     HeldOutputJack(response["uuid"], d["id"], d["color"], d["port"])
                 )
             return Message(response["uuid"], MessageType.UPDATE, state)
         if response["message"] == "HALT":
             return Message(response["uuid"], MessageType.HALT)
+        return None
 
     def create_directive(self, message: Message) -> bytes:
         """Inverse of ``parse_directive``"""
