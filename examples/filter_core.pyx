115f8c75146f17076003c2238ddda69413f87134
Daniel Gruss <dsgruss@users.noreply.github.com>
2022-06-01 15:51:27 -0700

    Add resonance to filter core

--- examples/filter_core.pyx
+++ examples/filter_core.pyx
@@ -3,10 +3,13 @@ import numpy as np
 from brain import BLOCK_SIZE, SAMPLE_RATE
 
 cdef class LadderFilter:
-    cdef double omega0, state[4], input, input_new, dt, pi
+    cdef double omega0, input, pi
+    cdef double state[4]
+    cdef double resonance
 
     def __init__(self):
         self.pi = np.pi
+        self.resonance = 1
         self.reset()
         self.setCutoff(0)
 
@@ -19,41 +22,47 @@ cdef class LadderFilter:
 
     cdef process(self, double input, double dt):
 
-        self.input_new = input
-        self.dt = dt
-        self.rk4(dt, self.state)
+        self.rk4(dt, self.state, self.input / 16000, input / 16000)
         self.input = input
-        return self.state[3]
+        return self.state[3] * 16000
 
-    cdef rk4(self, double dt, double *x):
-        cdef double yi[4], k1[4], k2[4], k3[4], k4[4]
+    cdef double clip(self, double x):
+        if x < -3.0:
+            x = -3.0
+        if x > 3.0:
+            x = 3.0
+        return x * (27 + x * x) / (27 + 9 * x * x)
 
-        # f(0, x, k1)
-        k1[0] = self.omega0 * (self.input - x[0])
-        k1[1] = self.omega0 * (x[0] - x[1])
-        k1[2] = self.omega0 * (x[1] - x[2])
-        k1[3] = self.omega0 * (x[2] - x[3])
+    cdef f(self, double t, double *x, double *dxdt, double input, double input_new, double dt):
+        cdef double inputt = input * (t / dt) + input_new * (1 - t / dt)
+        cdef double inputc = self.clip(inputt - self.resonance * x[3])
+        cdef double yc0 = self.clip(x[0])
+        cdef double yc1 = self.clip(x[1])
+        cdef double yc2 = self.clip(x[2])
+        cdef double yc3 = self.clip(x[3])
+
+        dxdt[0] = self.omega0 * (inputc - yc0)
+        dxdt[1] = self.omega0 * (yc0 - yc1)
+        dxdt[2] = self.omega0 * (yc1 - yc2)
+        dxdt[3] = self.omega0 * (yc2 - yc3)
+
+    cdef rk4(self, double dt, double *x, double input, double input_new):
+        cdef double yi[4]
+        cdef double k1[4]
+        cdef double k2[4]
+        cdef double k3[4]
+        cdef double k4[4]
+
+        self.f(0, x, k1, input, input_new, dt)
         for i in range(4):
             yi[i] = x[i] + k1[i] * dt / 2
-        # f(dt / 2, yi, k2)
-        k2[0] = self.omega0 * ((self.input + self.input_new) / 2 - yi[0])
-        k2[2] = self.omega0 * (yi[1] - yi[2])
-        k2[1] = self.omega0 * (yi[0] - yi[1])
-        k2[3] = self.omega0 * (yi[2] - yi[3])
+        self.f(dt / 2, yi, k2, input, input_new, dt)
         for i in range(4):
             yi[i] = x[i] + k2[i] * dt / 2
-        # f(dt / 2, yi, k3)
-        k3[0] = self.omega0 * ((self.input + self.input_new) / 2 - yi[0])
-        k3[2] = self.omega0 * (yi[1] - yi[2])
-        k3[1] = self.omega0 * (yi[0] - yi[1])
-        k3[3] = self.omega0 * (yi[2] - yi[3])
+        self.f(dt / 2, yi, k3, input, input_new, dt)
         for i in range(4):
             yi[i] = x[i] + k3[i] * dt
-        # f(dt, yi, k4)
-        k4[0] = self.omega0 * (self.input_new - yi[0])
-        k4[2] = self.omega0 * (yi[1] - yi[2])
-        k4[1] = self.omega0 * (yi[0] - yi[1])
-        k4[3] = self.omega0 * (yi[2] - yi[3])
+        self.f(dt, yi, k4, input, input_new, dt)
         for i in range(4):
             x[i] += dt * (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]) / 6
 
