580ccdc25b748d4955f96dc405b01b12412edb4e
Daniel Gruss <dsgruss@users.noreply.github.com>
2022-06-04 12:04:47 -0700

    Add process tracking and some window management

--- examples/manager.py
+++ examples/manager.py
@@ -5,7 +5,7 @@ import multiprocessing
 import queue
 import tkinter as tk
 
-from collections import Counter
+from collections import defaultdict
 
 import brain
 
@@ -26,8 +26,9 @@ class Manager:
 
     def __init__(self, loop: asyncio.AbstractEventLoop):
         self.loop = loop
+        self.id = "root:virtual_examples:manager"
 
-        self.mod = brain.Module(self.name, ManagerEventHandler(self))
+        self.mod = brain.Module(self.name, ManagerEventHandler(self), id=self.id)
 
         self.colors = [
             ("cyan", str(36) + ";1", 180),
@@ -51,7 +52,7 @@ class Manager:
         self.gridx = 4
         self.gridy = 0
         self.color_idx = 0
-        self.process_counter = Counter()
+        self.open_processes = defaultdict(dict)
         self.events = multiprocessing.Queue()
         self.printer = honcho.printer.Printer(width=20)
         self.snapshots = {}
@@ -73,13 +74,13 @@ class Manager:
 
         tk.Label(self.root, text=self.name).place(x=10, y=10)
         tk.Button(
-            self.root, text="🔌    Close All", command=self.mod.halt_all, width=22
+            self.root, text="🔌    Close All", command=self.close_all, width=22
         ).place(x=10, y=50)
         tk.Button(
-            self.root, text="Get Snapshots", command=self.get_snapshots, width=22
+            self.root, text="Save Preset", command=self.get_snapshots, width=22
         ).place(x=10, y=80)
         tk.Button(
-            self.root, text="Set Snapshots", command=self.set_snapshots, width=22
+            self.root, text="Load Preset", command=self.set_snapshots, width=22
         ).place(x=10, y=110)
 
         for i, process in enumerate(self.processes):
@@ -123,27 +124,36 @@ class Manager:
                 logging.info("%s started (pid=%s)" % (msg.name, msg.data["pid"]))
             elif msg.type == "stop":
                 logging.info("%s stopped (rc=%s)" % (msg.name, msg.data["returncode"]))
+                id = msg.name.split(".")[0]
+                id_idx = int(msg.name.split(".")[1])
+                del self.open_processes[id][id_idx]
 
     def launch(self, dest, id):
-        p = multiprocessing.Process(
-            target=honcho.process.Process(
-                [
-                    "python",
-                    dest,
-                    "--gridx",
-                    str(self.gridx),
-                    "--gridy",
-                    str(self.gridy),
-                    "--color",
-                    str(self.colors[self.color_idx][2]),
-                    "--id",
-                    str(self.process_counter[id]),
-                ],
-                name=id + "." + str(self.process_counter[id]),
-                colour=self.colors[self.color_idx][1],
-            ).run,
-            args=(self.events,),
+        # Find the next open id the slow way
+        id_idx = 0
+        used_idxs = self.open_processes[id].keys()
+        while id_idx in used_idxs:
+            id_idx += 1
+
+        target = honcho.process.Process(
+            [
+                "python",
+                dest,
+                "--gridx",
+                str(self.gridx),
+                "--gridy",
+                str(self.gridy),
+                "--color",
+                str(self.colors[self.color_idx][2]),
+                "--id",
+                str(id_idx),
+            ],
+            name=id + "." + str(id_idx),
+            colour=self.colors[self.color_idx][1],
         )
+        self.open_processes[id][id_idx] = target
+
+        p = multiprocessing.Process(target=target.run, args=(self.events,))
         p.start()
 
         self.gridx += 4
@@ -151,7 +161,11 @@ class Manager:
             self.gridx = 4
             self.gridy += 10
         self.color_idx = (self.color_idx + 1) % len(self.colors)
-        self.process_counter[id] += 1
+
+    def close_all(self):
+        self.mod.halt_all()
+        self.gridx = 4
+        self.gridy = 0
 
     def shutdown(self):
         for task in asyncio.all_tasks():
@@ -168,7 +182,8 @@ class Manager:
         self.mod.get_all_snapshots()
 
     def recieved_snapshot(self, uuid, data, patched):
-        self.snapshots[uuid] = (data, patched)
+        if uuid != self.id:
+            self.snapshots[uuid] = (data, patched)
 
     def set_snapshots(self):
         for k, v in self.snapshots.items():
